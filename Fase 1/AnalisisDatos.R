9c8012fdc75f7749709eb066d806d22060904730
Nicolas Poch <n.poch@uniandes.edu.co>
2021-03-04 11:05:59 -0500

    Método de monte carlo para P de percepción

--- Fase 1/AnalisisDatos.R
+++ Fase 1/AnalisisDatos.R
@@ -9,19 +9,26 @@ Casos_Reportados_Kennedy <- read_excel("Casos_Reportados.xlsx",sheet = "Casos Re
 Casos_Reportados_Bosa <- read_excel("Casos_Reportados.xlsx",sheet = "Casos Reportados Bosa")
 Casos_Reportados_Suba <- read_excel("Casos_Reportados.xlsx",sheet = "Casos Reportados Suba")
 Validacion_Utilidades <- read_excel("Validacion_Utilidades.xlsx",col_types = c("date", "numeric"))
+difk<-as.numeric(difftime(Casos_Reportados_Kennedy$Fecha,lag(Casos_Reportados_Kennedy$Fecha),units = c("hours")))
+difk2<-hour(Casos_Reportados_Kennedy$Fecha)
 #--------------Kennedy-------------------------
+#Evaluar con p-value
 cantidad<-rep(1,length(Casos_Reportados_Kennedy))
+
 Casos_Reportados_Kennedy<-cbind(Casos_Reportados_Kennedy,cantidad)
 Casos_Reportados_Kennedy$Hora<-hour(Casos_Reportados_Kennedy$Fecha)
 Casos_Reportados_Kennedy$Dia<-day(Casos_Reportados_Kennedy$Fecha)
 Casos_Reportados_Kennedy$Mes<-month(Casos_Reportados_Kennedy$Fecha)
 Casos_Reportados_Kennedy$Ano<-year(Casos_Reportados_Kennedy$Fecha)
+
 Casos_Reportados_Kennedy_Hora<-aggregate(x=Casos_Reportados_Kennedy$cantidad,by=list(Casos_Reportados_Kennedy$Hora,Casos_Reportados_Kennedy$Dia,Casos_Reportados_Kennedy$Mes,Casos_Reportados_Kennedy$Ano),FUN=sum)
+
 colnames(Casos_Reportados_Kennedy_Hora)<-list("Hora","Dia","Mes","Ano","Casos")
+
 plotdist(Casos_Reportados_Kennedy_Hora$Casos, histo=TRUE, demp=TRUE)
 fdst<-fitdist(Casos_Reportados_Kennedy_Hora$Casos,distr = "exp")
 tempEst<-gofstat(fdst)
-summary(fdst)
+print(tempEst)
 #--------------Bosa-------------------------------------------------------------
 cantidad<-rep(1,length(Casos_Reportados_Bosa))
 Casos_Reportados_Bosa<-cbind(Casos_Reportados_Bosa,cantidad)
@@ -49,32 +56,94 @@ fdst<-fitdist(Casos_Reportados_Suba_Hora$Casos,distr = "exp")
 tempEst<-gofstat(fdst)
 summary(fdst)
 #-------------Encuesta de Seguridad--------------------------------------------
-infoEncuesta<-matrix(0,ncol = 4,nrow = 1)
+#Por Métodos de montecarlo
+infoEncuesta<-matrix(0,ncol = 5,nrow = 1)
 infoEncuesta<-data.frame(infoEncuesta)
-colnames(infoEncuesta)<-list("index","Estado N","Estado N+1","Delta")
+colnames(infoEncuesta)<-list("Estado N Percepción","Estado N Policías","Estado N+1 Percepción","Estado N+1 Policías","Freq")
 dLength<-length(Encuesta_Seguridad$Semana)
+
 for(i in 1:dLength)
 {
-  temp<-data.frame(list(i,Encuesta_Seguridad[i,"Percepción"],Encuesta_Seguridad[i+1,"Percepción"],Encuesta_Seguridad[i+1,"Número de policías"]-Encuesta_Seguridad[i,"Número de policías"]))
-  colnames(temp)<-list("index","Estado N","Estado N+1","Delta")
+  temp<-data.frame(list(Encuesta_Seguridad[i,"Percepción"],round((Encuesta_Seguridad[i,"Número de policías"]-100)/500),Encuesta_Seguridad[i+1,"Percepción"],(Encuesta_Seguridad[i+1,"Número de policías"]-100)/500),1)
+  colnames(temp)<-list("Estado N Percepción","Estado N Policías","Estado N+1 Percepción","Estado N+1 Policías","Freq")
   infoEncuesta<-rbind(infoEncuesta,temp)
 }
 infoEncuesta<-infoEncuesta[2:dLength,]
+infoEncuesta$`Estado N Policías`<-round(infoEncuesta$`Estado N Policías`,digits = 2)
+infoEncuesta$`Estado N+1 Policías`<-round(infoEncuesta$`Estado N Policías`,digits = 2)
+
+infoEncuestaAgg<-aggregate(x=infoEncuesta$Freq,by=list(infoEncuesta$`Estado N Percepción`,infoEncuesta$`Estado N Policías`,infoEncuesta$`Estado N+1 Percepción`,infoEncuesta$`Estado N+1 Policías`),FUN=sum)
+colnames(infoEncuestaAgg)<-list("Estado N Percepción","Estado N Policías","Estado N+1 Percepción","Estado N+1 Policías","Freq")
+
+probabilidadesNN1<-matrix(0,ncol=5,nrow = 1)
+probabilidadesNN1<-data.frame(probabilidadesNN1)
+colnames(probabilidadesNN1)<-list("Percepcion N","Policias N","Percepcion N+1","Policias N+1","Prob")
+for(pn in unique(infoEncuestaAgg$`Estado N Percepción`))
+{
+  tempPerc<-infoEncuestaAgg[infoEncuestaAgg$`Estado N Percepción`==pn,]
+  for(cn in unique(infoEncuestaAgg$`Estado N Policías`))
+  {
+    tempPercPol<-tempPerc[tempPerc$`Estado N Policías`==cn,]
+    totalEv<-sum(tempPercPol$Freq)
+    for(pn1 in unique(infoEncuestaAgg$`Estado N+1 Percepción`))
+    {
+      tempPercPolPerc1<-tempPerc[tempPercPol$`Estado N+1 Percepción`==pn1,]
+      print(tempPercPolPerc1)
+      for(cn1 in unique(infoEncuestaAgg$`Estado N+1 Policías`))
+      {
+        tempPercPolPerc1Pol1<-tempPercPolPerc1[tempPercPolPerc1$`Estado N Policías`,]
+        ansP<-0
+        if(length(tempPercPolPerc1Pol1$Freq)!=0)
+        {
+          ansP<-sum(tempPercPol$Freq)/totalEv
+        }
+        ans<-data.frame(list(pn,cn,pn1,cn1,ansP))
+        colnames(ans)<-list("Percepcion N","Policias N","Percepcion N+1","Policias N+1","Prob")
+        probabilidadesNN1<-rbind(probabilidadesNN1,ans)
+      }
+    }
+  }
+}
+View(probabilidadesNN1)
+
+Percepciones<-unique(infoEncuesta$`Estado N Percepción`)
+CantidadPolicias<-0:5
+CantidadPoliciasXPercepciones<-expand.grid(CantidadPolicias,Percepciones)
+S_percepcion<-c()
+for(i in 1:18)
+{
+  S_percepcion<-c(S_percepcion,c(paste(CantidadPoliciasXPercepciones$Var1, CantidadPoliciasXPercepciones$Var2, sep = ",")))
+}
+
+
+
+
+
+
 estadoNBueno<-infoEncuesta[infoEncuesta$`Estado N`=="Buena",]
 estadoNRegular<-infoEncuesta[infoEncuesta$`Estado N`=="Regular",]
 estadoNMalo<-infoEncuesta[infoEncuesta$`Estado N`=="Mala",]
 
-descdist(estadoNBueno$Delta)
-descdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Buena"])
-descdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Regular"])
-descdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Mala"])
+plotdist(estadoNBueno$Delta,histo = T,demp = T,breaks = 20)
+plotdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Buena"],histo = T,demp = T)
+plotdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Regular"],histo = T,demp = T)
+plotdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Mala"],histo = T,demp = T)
 
-descdist(estadoNRegular$Delta)
-descdist(estadoNRegular$Delta[estadoNRegular$`Estado N+1`=="Buena"])
-descdist(estadoNRegular$Delta[estadoNRegular$`Estado N+1`=="Regular"])
-descdist(estadoNRegular$Delta[estadoNRegular$`Estado N+1`=="Mala"])
+plotdist(Encuesta_Seguridad$`Número de policías`[Encuesta_Seguridad$Percepción=="Buena"],histo = T,demp = T,breaks = 20)
+plot(fitdist(Encuesta_Seguridad$`Número de policías`[Encuesta_Seguridad$Percepción=="Buena"],"norm"))
+summary(fitdist(Encuesta_Seguridad$`Número de policías`[Encuesta_Seguridad$Percepción=="Buena"],"norm"))
 
-descdist(estadoNMalo$Delta)
-descdist(estadoNMalo$Delta[estadoNMalo$`Estado N+1`=="Buena"])
-descdist(estadoNMalo$Delta[estadoNMalo$`Estado N+1`=="Regular"])
-descdist(estadoNMalo$Delta[estadoNMalo$`Estado N+1`=="Mala"])
+descdist(estadoNBueno$Delta,boot=1000)
+descdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Buena"],boot=1000)
+descdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Regular"],boot=1000)
+descdist(estadoNBueno$Delta[estadoNBueno$`Estado N+1`=="Mala"],boot=1000)
+
+plot(fitdist((estadoNRegular$Delta),"pois"),breaks=10)
+plot(fitdist(estadoNRegular$Delta[estadoNRegular$`Estado N+1`=="Buena"],"norm"),breaks=10)
+plot(fitdist(estadoNRegular$Delta[estadoNRegular$`Estado N+1`=="Regular"],"norm"),breaks=10)
+plot(fitdist(estadoNRegular$Delta[estadoNRegular$`Estado N+1`=="Buena"],"norm"),breaks=10)
+
+plot(fitdist(estadoNMalo$Delta,"norm"),breaks=10)
+plot(fitdist(estadoNMalo$Delta[estadoNMalo$`Estado N+1`=="Buena"],"norm"),breaks=10)
+plot(fitdist(estadoNMalo$Delta[estadoNMalo$`Estado N+1`=="Regular"],"norm"),breaks=10)
+plot(fitdist(estadoNMalo$Delta[estadoNMalo$`Estado N+1`=="Mala"],"norm"),breaks=10)
