f9e81744f9ee87ae2b4d88c62052f797900071f4
Nicolas Poch <n.poch@uniandes.edu.co>
2021-04-14 22:42:31 -0500

    Final

--- Fase 2/Punto 1/Punto 1/Analisis.R
+++ Fase 2/Punto 1/Punto 1/Analisis.R
@@ -1,24 +1,28 @@
-#---------------Paquetes------------------
+#---------------Paquetes----------------------------------------
 library(expm)
 library(markovchain)
-#---------------Variables-----------------
+#---------------Variables---------------------------------------
 #Tasas
 lambda_llegada<-0.4
 lambda_123<-1/25
 lambda_policia<-1/(40/60)
 lambda_uri<-1
+#servidores
+S_123<-3
+S_policia<-3
+S_uri<-4
 #capacidades
-c_123<-4
-c_policia<-2
-c_uri<-5
+C_123<-1
+C_policia<-2
+C_uri<-1
 #---------------------Cadena de Markov CMTC---------------------
 #Estados
 S_sistema<-c()
-for(s_123 in 0:c_123)
+for(s_123 in 0:(C_123+S_123))
 {
-  for(s_policia in 0:c_policia)
+  for(s_policia in 0:(C_policia+S_policia))
   {
-    for(s_uri in 0:c_uri)
+    for(s_uri in 0:(C_uri+S_uri))
     {
       S_sistema<-c(S_sistema,paste(s_123,s_policia,s_uri,sep = ","))
     }
@@ -45,27 +49,27 @@ for(X_n in S_sistema)
     #Estaba en 123 y se va del sistema 
     if((X_n1_123==X_n_123-1) && (X_n1_uri==X_n_uri) && (X_n1_policia==X_n_policia))
     {
-      matrizQ[X_n,X_n1]<-X_n_123*lambda_123*0.76
+      matrizQ[X_n,X_n1]<-min(X_n_123,S_123)*lambda_123*0.76
     }
     #Estaba en 123 y pasa a policía
     if((X_n1_123==X_n_123-1) && (X_n1_uri==X_n_uri) && (X_n1_policia==X_n_policia+1))
     {
-      matrizQ[X_n,X_n1]<-X_n_123*lambda_123*0.24
+      matrizQ[X_n,X_n1]<-min(X_n_123,S_123)*lambda_123*0.24
     }
     #Estaba en policía y se va del sistema
     if((X_n1_123==X_n_123) && (X_n1_uri==X_n_uri) && (X_n1_policia==X_n_policia-1))
     {
-      matrizQ[X_n,X_n1]<-X_n_policia*lambda_policia*0.5
+      matrizQ[X_n,X_n1]<-min(X_n_policia,S_policia)*lambda_policia*0.5
     }
     #Estaba en policía y se va a uri
     if((X_n1_123==X_n_123) && (X_n1_uri==X_n_uri+1) && (X_n1_policia==X_n_policia-1))
     {
-      matrizQ[X_n,X_n1]<-X_n_policia*lambda_policia*0.35
+      matrizQ[X_n,X_n1]<-min(X_n_policia,S_policia)*lambda_policia*0.35
     }
     #Estaba en uri y se va del sistema
     if((X_n1_123==X_n_123) && (X_n1_uri==X_n_uri-1) && (X_n1_policia==X_n_policia))
     {
-      matrizQ[X_n,X_n1]<-X_n_policia*lambda_uri
+      matrizQ[X_n,X_n1]<-min(X_n_uri,S_uri)*lambda_uri
     }
   }
 }
@@ -79,12 +83,12 @@ Embebida<-generatorToTransitionMatrix(matrizQ)
 #------------------Medidas de desempeño----------------------
 #Primera parte
 #Considerando que el sistema empieza en el estado 0,0,0
-tiempoParaOcupado<-ExpectedTime(CMTC,1,90)
+tiempoParaOcupado<-ExpectedTime(CMTC,1,180)
 print(paste("El tiempo (horas) que toma el sistema para llegar a máxima capacidad es: ",tiempoParaOcupado,sep = ""))
 #Segunda parte
 h<-8
 alhphaInicial<-rep(0,nrow(matrizQ))
-alhphaInicial[73]<-1
+alhphaInicial[145]<-1
 L<--1/diag(matrizQ)
 matrizMultiplicar<-Embebida
 contador<-1
@@ -106,7 +110,7 @@ M_t<-alhphaInicial%*%matrizOcupacion*L
 ans<-0
 for(s in S_sistema)
 {
-  if(as.numeric(strsplit(X_n1,split=",")[[1]][2])==c_policia)
+  if(as.numeric(strsplit(s,split=",")[[1]][2])==(C_policia+S_policia))
   {
     ans<-ans+M_t[1,s][[1]]
   }
